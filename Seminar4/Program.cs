58f7b69651f791600a04a0a36c57a7f2ed6f6f92
Alexey < >
2023-08-05 15:24:23 +0300

    Расширено решение задач

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -2,39 +2,93 @@
 /*
 
 */
+//Используемые функции
+/*
+int RequestNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Вы ввели число: " + number);
+    return number;
+}
+*/
 //Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 /*
-Console.Write("Введите ваше число и степень, в которую вы хотите возвести это число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int degree = Convert.ToInt32(Console.ReadLine());
-double res = Math.Pow(num, degree);
-Console.Write(res);
+void constructionCycle(int baseNum, int degree)
+{
+    int current = baseNum;
+    Console.WriteLine($"Ход работы программы по возведению числа {baseNum} в степень {degree}: ");
+    for(int i = 0; i < degree; i++)
+    {
+        Console.Write(current + " ");
+        current = current * baseNum;
+    }
+    Console.WriteLine("");
+}
+
+int baseNum = RequestNumber("Введите ваше число: ");
+int degree = RequestNumber("Введите степень, в которую вы хотите возвести это число: ");
+constructionCycle(baseNum, degree);
+Console.WriteLine($"Число {baseNum} в степени {degree} равно {Math.Pow(baseNum, degree)}");
 */
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 /*
-Console.Write("Введите ваше число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int res = 0, checkedNum = num;
-while(checkedNum != 0)
+int SearchSum(int num)
 {
-    res = res + checkedNum % 10;
-    checkedNum = checkedNum / 10;
+    int sum = 0, current = num;
+    while(current != 0)
+    {
+        sum = sum + current % 10;
+        current = current / 10;
+    }
+    return sum;
 }
-Console.Write("Сумма цифр вашего числа равно: " + res);
+
+int num = RequestNumber("Введите ваше число: ");
+int res = SearchSum(num);
+Console.WriteLine("Сумма цифр вашего числа равно: " + res);
 */
 //Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
 /*
-Console.Write("Введите размер массива: ");
-int count = Convert.ToInt32(Console.ReadLine());
-int [] mas = new int[count];
-Console.Write("Задайте массив: ");
-for(int i = 0; i < count; i++)
+int [] fillingTheArray(int length, int operation)
 {
-    mas[i] = Convert.ToInt32(Console.ReadLine());
+    int [] array = new int[length];
+    if(operation == 0)
+    {
+        Console.Write("Задайте массив: ");
+        for(int i = 0; i < length; i++)
+        {
+            array[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+    else
+    {
+        Console.Write("Задайте нижний и верхний предел, в котором будут выбираться случайные числа: ");
+        int min = Convert.ToInt32(Console.ReadLine());
+        int max = Convert.ToInt32(Console.ReadLine());
+        for(int i = 0; i < length; i++)
+        {
+            array[i] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
 }
-Console.Write("Введенный вами массив: ");
-for(int i = 0; i < count; i++)
+void outputRes(int [] mas, int operation, int length)
 {
-    Console.Write(mas[i] + " ");
+    if(operation == 1)
+        Console.Write("Полученный массив из случайных чисел: [");
+    else
+        Console.Write("Введенный вами массив: [");
+    for(int i = 0; i < length - 1; i++)
+    {
+        Console.Write(mas[i] + ", ");
+    }
+    Console.Write(mas[length - 1] + "]");
 }
+
+int length = RequestNumber("Введите размер массива: ");
+Console.Write("Введите 1, чтобы заполнить массив случайыми числами, или 0, чтобы заполнить его самостоятельно: ");
+int operation = Convert.ToInt32(Console.ReadLine());
+int [] mas = fillingTheArray(length, operation);
+outputRes(mas, operation, length);
 */
