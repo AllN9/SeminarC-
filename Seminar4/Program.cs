70d4ca4dfb58c720be69e6314a3cd0f265f75ffd
Alexey < >
2023-08-05 10:45:30 +0300

    Добавлено решение задачи 27

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -7,11 +7,19 @@
 
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-
-
+/*
+Console.Write("Введите ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0, checkedNum = num;
+while(checkedNum != 0)
+{
+    res = res + checkedNum % 10;
+    checkedNum = checkedNum / 10;
+}
+Console.Write("Сумма цифр вашего числа равно: " + res);
+*/
 //Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
-
+/*
 Console.Write("Введите размер массива: ");
 int count = Convert.ToInt32(Console.ReadLine());
 int [] mas = new int[count];
@@ -25,3 +33,4 @@ for(int i = 0; i < count; i++)
 {
     Console.Write(mas[i] + " ");
 }
+*/
