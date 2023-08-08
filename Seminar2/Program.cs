c520bdce8fed020ab46e1291f2affb73c0259854
Alexey < >
2023-08-08 17:33:38 +0300

    Добавлены коментарии к 1, 2 и 3 семинару

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -15,36 +15,36 @@ Console.Write("Вначале было число " + randNumber);
 */
 //Используемые функции
 /*
-int RequestТumber(int nomberOfTask)
+int RequestТumber(int nomberOfTask)//Функция запроса данных у пользователя
 {
 
-    if (nomberOfTask == 1)
+    if (nomberOfTask == 1)//Определение вывода приветственной фразы для выбранной задачи
         Console.Write("Hellow!\nInput your three-digit number: ");
     else
         Console.Write("Hellow!\nInput your number: ");
     int requestedNumber = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("You entered a number " + requestedNumber);
+    Console.WriteLine("You entered a number " + requestedNumber);//Вывод введенного числа для проверки пользователем
     return requestedNumber;
 }
-int ErrorChecking(int nomberOfTask, int requestedNumber)
+int ErrorChecking(int nomberOfTask, int requestedNumber)//Функция избежания ошибок
 {
     int correctRequestedNumber = requestedNumber;
-    if (nomberOfTask == 1)
-        while(correctRequestedNumber < 100 || correctRequestedNumber > 999)
+    if (nomberOfTask == 1)//Проверка ошибок первой задачи
+        while(correctRequestedNumber < 100 || correctRequestedNumber > 999)//Определение, является ли число трехзначным
         {
             Console.WriteLine("You entered an incorrect number");
             Console.Write("Input your correct three-digit number: ");
             correctRequestedNumber = Convert.ToInt32(Console.ReadLine());
         }
         else
-            if(nomberOfTask == 2)
+            if(nomberOfTask == 2)//Проверка ошибок второй задачи
             {
-                if(correctRequestedNumber < 100)
-                    Console.WriteLine("you entered a two-digit number\nThe third digit was: -1");
-                return correctRequestedNumber;
+                if(correctRequestedNumber < 100)//Определение, является ли число двузначным
+                    Console.WriteLine("You entered a two-digit number");
+                return -1;
             }
-            else
-                while(correctRequestedNumber > 7 || correctRequestedNumber < 1)
+            else//Проверка ошибок третьей задачи
+                while(correctRequestedNumber > 7 || correctRequestedNumber < 1)//Определение, является ли число номером дня недели
                 {
                     Console.WriteLine("Your number is not the day of the week");
                     Console.Write("Input a number from 1 to 7: ");
@@ -56,22 +56,22 @@ int ErrorChecking(int nomberOfTask, int requestedNumber)
 //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и 
 //на выходе показывает вторую цифру этого числа.
 /*
-int number1 = RequestТumber(1);
+int number1 = RequestТumber(1);//Запрос трехзначного числа
 
-int correctNumber1 = ErrorChecking(1, number1);
+int correctNumber1 = ErrorChecking(1, number1);//Проверка, является ли число трехзначным
 
-correctNumber1 = ((correctNumber1 / 10) % 10);
+correctNumber1 = ((correctNumber1 / 10) % 10);//Определение и вывод второй цифры заданного числа
 Console.WriteLine("The second digit was: " + correctNumber1);
 */
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа ( или -1, если третьей цифры нет).
 /*
-int number2 = RequestТumber(2);
+int number2 = RequestТumber(2);//Запрос числа
 
-int correctNumber2 = ErrorChecking(2, number2);
+int correctNumber2 = ErrorChecking(2, number2);//Проверка условия задачи
 
-while(correctNumber2 > 999)
+while(correctNumber2 > 999)//Поиск третьей цифры
     correctNumber2 = correctNumber2 / 10;
-int third = (correctNumber2 % 10);
+int third = (correctNumber2 % 10);//Определение третьей цифры
 Console.WriteLine("The third digit was: " + third);
 */
 //Задача 15: Напишите программу, которая принимает на вход цифру, 
@@ -79,11 +79,11 @@ Console.WriteLine("The third digit was: " + third);
 /*
 string[] daysOfWeek = { "Sunday", "Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday" };
 
-int number3 = RequestТumber(3);
+int number3 = RequestТumber(3);//Запрос числа
 
-int correctNumber3 = ErrorChecking(3, number3);
+int correctNumber3 = ErrorChecking(3, number3);//Проверка коректности ввода
 
-if(correctNumber3 == 6 || correctNumber3 == 7)
+if(correctNumber3 == 6 || correctNumber3 == 7)//Вывод результата согласно условию
     Console.WriteLine($"your day of the week is {daysOfWeek[correctNumber3 - 1] } and it's a weekend");
 else
     Console.WriteLine($"your day of the week is {daysOfWeek[correctNumber3 - 1] } and it's not a weekend");
@@ -168,9 +168,9 @@ void TaskSolution(int numberOfTask, int correctRequestedNumber)
 }
 
 Console.WriteLine("Hello!");
-int numberOfTask = Greeting(), requestedNumber, correctRequestedNumber;
+int numberOfTask = Greeting(), requestedNumber, correctRequestedNumber;//Начальное определение действий программы
 
-while(numberOfTask != 0)
+while(numberOfTask != 0)//Цыкл выбора желаемой задачи
 {
     requestedNumber = RequestТumber(numberOfTask);//Запрос числа для выбранной задачи
     correctRequestedNumber = ErrorChecking(numberOfTask, requestedNumber);//Проверка корректности ввода
